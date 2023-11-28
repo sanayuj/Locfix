0d45dd7ec0c9528873efc79a8ba20b71ee0ede71
sanayuj <sanayuj2255@gmail.com>
2023-11-28 16:10:05 +0530

    add constants

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,8 +1,9 @@
 import React,{useState,useEffect} from 'react'
 import axios from '../../axios'
+import { imageUrl } from '../../constants/constants'
 
 function Cards(props) {
-    const [movie,setmovie]=useState({})
+    const [movie,setmovie]=useState([])
     useEffect(()=>{
         axios.get(props.url).then((response)=>{
             console.log(response.data.results,"RES!!")
@@ -12,18 +13,14 @@ function Cards(props) {
   return (
     <> <h1 className=' my-5 mx-4 '>Trending </h1>
     <div className='container d-flex'>
-    <div className="card  mx-4"   style={{width: "18rem"}}>
-    <img src="..." className="card-img-top" alt="..."/>
+    {movie.map((obj)=>(
+    <div key={obj.id} className="card  mx-4"   style={{width: "18rem"}}>
+    <img src={`${imageUrl+obj.poster_path}`} className="card-img-top" alt="unable to load"/>
     <div className="card-body">
       <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     </div>
   </div>
-  <div className="card "   style={{width: "18rem"}}>
-    <img src="..." className="card-img-top" alt="..."/>
-    <div className="card-body">
-      <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    </div>
-  </div>
+  ))}
   </div>
   </>
   )
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,4 @@
 export const baseURL="https://api.themoviedb.org/3"
 export const API_key="686a64462c764e78162ff07be68e5ab9"
+
+export const imageUrl="https://image.tmdb.org/t/p/original"
